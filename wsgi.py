8cd8077986099aac06eb18089d56d69efe97981e
Patrick Mwila <patrickmwila.org@gmail.com>
2024-12-13 08:13:03 +0200

    Updated Deployment Configs

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 from app import app
+
+# This is the variable that mod_wsgi is looking for
+application = app
 
 if __name__ == "__main__":
     app.run()
